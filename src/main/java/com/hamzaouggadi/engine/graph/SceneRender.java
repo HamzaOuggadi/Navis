a68debdb69cbf90c5567c73242f617b4e310daa7
Hamza Ouggadi <45943796+HamzaOuggadi@users.noreply.github.com>
2024-01-01 17:35:41 +0100

    Rendered first cube with zoom and movement ability

--- src/main/java/com/hamzaouggadi/engine/graph/SceneRender.java
+++ src/main/java/com/hamzaouggadi/engine/graph/SceneRender.java
@@ -1,8 +1,10 @@
 package com.hamzaouggadi.engine.graph;
 
+import com.hamzaouggadi.engine.scene.Entity;
 import com.hamzaouggadi.engine.scene.Scene;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import static org.lwjgl.opengl.GL30.*;
@@ -28,12 +30,20 @@ public class SceneRender {
     public void render(Scene scene) {
         shaderProgram.bind();
 
-        scene.getMeshMap().values().forEach(mesh -> {
-            glBindVertexArray(mesh.getVaoId());
-            glDrawElements(GL_TRIANGLES, mesh.getNumVertices(), GL_UNSIGNED_INT, 0);
-        });
-
         uniformsMap.setUniform("projectionMatrix", scene.getProjection().getProjMatrix());
+
+        Collection<Model> models = scene.getModelMap().values();
+
+        for (Model model : models) {
+            model.getMeshList().stream().forEach(mesh -> {
+                glBindVertexArray(mesh.getVaoId());
+                List<Entity> entities = model.getEntitiesList();
+                for (Entity entity : entities) {
+                    uniformsMap.setUniform("modelMatrix", entity.getModelMatrix());
+                    glDrawElements(GL_TRIANGLES, mesh.getNumVertices(), GL_UNSIGNED_INT, 0);
+                }
+            });
+        }
 
         glBindVertexArray(0);
         shaderProgram.unbind();
@@ -42,6 +52,7 @@ public class SceneRender {
     private void createUniforms() {
         uniformsMap = new UniformsMap(shaderProgram.getProgramId());
         uniformsMap.createUniform("projectionMatrix");
+        uniformsMap.createUniform("modelMatrix");
     }
 
 }
